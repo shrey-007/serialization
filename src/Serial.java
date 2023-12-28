ddc874b91b9d72d7305da30c0618b8066baf4b50
shreySaxena <shreyyerhs07@gmail.com>
2023-12-28 13:37:29 +0530

    added explaination

--- src/Serial.java
+++ src/Serial.java
@@ -8,13 +8,14 @@ public class Serial {
         Student student=new Student("shrey","shreyyerhs07@gmail.com",20,"gwalior");
 
         try {
-
+            //create a fileOutputStream object to send whatever raw data is present in stream to file object.txt
             FileOutputStream fileOutputStream=new FileOutputStream("object.txt");
-
+            //to convert object into stream of bytes and sending these bytes to fileOutputStream
             ObjectOutputStream objectOutputStream=new ObjectOutputStream(fileOutputStream);
-
+            //method to write bytes into file
             objectOutputStream.writeObject(student);
-
+            //so overall objectOutputStream converts the state of an object to stream of bytes(fileOutputStream)
+            //and then fileOutputStream is writing those bytes in object.txt
             System.out.println("Object is transfered to file");
         }
         catch (Exception e){
